34c9698b6ae027f93bc767112fd7b0d2c381d554
UshaRani-06 <45165347+UshaRani-06@users.noreply.github.com>
2018-11-26 16:52:02 +0530

    Add Genre Controller

---
+++ QuirkyBookRentals/QuirkyBookRentals/Controllers/GenreController.cs
@@ -0,0 +1,30 @@
+﻿using QuirkyBookRentals.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuirkyBookRentals.Controllers
+{
+    public class GenreController : Controller
+    {
+        private ApplicationDbContext db;
+
+        public GenreController()
+        {
+            db = new ApplicationDbContext();
+        }
+        // GET: Genre
+        public ActionResult Index()
+        {
+            return View(db.Gneres.ToList());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+
+    }
+}
